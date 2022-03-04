f95aed5a29ac3a1dfae6049ae10b1d16ca574ba5
bxxmi <bmi6638@gmail.com>
2022-03-04 18:45:10 +0900

    feat: add input component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/GlobalStyle.module.css';
 import Header from './components/Header/Header';
+import Input from './components/Input/Input';
 
 export default function App({ $target }) {
   const $container = document.createElement('div');
@@ -8,4 +9,5 @@ export default function App({ $target }) {
   $target.appendChild($container);
 
   const header = new Header({ $container });
+  const input = new Input({ $container });
 }
