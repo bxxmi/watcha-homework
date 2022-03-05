b7623e1cfc6579d0a041a2de59faa965ccf6b47e
bxxmi <bmi6638@gmail.com>
2022-03-05 12:02:44 +0900

    feat: implement debounce logic

---
+++ src/utils/debounce.js
@@ -0,0 +1,11 @@
+export const debounce = (func, time) => {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, time);
+  };
+};
