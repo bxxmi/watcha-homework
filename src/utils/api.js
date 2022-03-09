36d088ebf5ba4ca928b79b0cd4ed57b49e233659
bxxmi <bmi6638@gmail.com>
2022-03-09 15:20:20 +0900

    refactor: add use strict, remove try-catch

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const API_ADDRESS =
   'https://5qfov74y3c.execute-api.ap-northeast-2.amazonaws.com/web-front/autocomplete';
 
@@ -5,15 +7,15 @@ export const requestKeyword = async (keyword) => {
   if (sessionStorage.getItem(`result_${keyword}`)) {
     return JSON.parse(sessionStorage.getItem(`result_${keyword}`));
   } else {
-    try {
-      const response = await fetch(`${API_ADDRESS}?value=${keyword}`);
+    const response = await fetch(`${API_ADDRESS}?value=${keyword}`);
+
+    if (response.ok) {
       const json = await response.json();
-
       sessionStorage.setItem(`result_${keyword}`, JSON.stringify(json));
-
-      return JSON.parse(sessionStorage.getItem(`result_${keyword}`));
-    } catch (error) {
-      console.log(error);
+    } else {
+      throw Error('API 연결에 실패했습니다');
     }
+
+    return JSON.parse(sessionStorage.getItem(`result_${keyword}`));
   }
 };
